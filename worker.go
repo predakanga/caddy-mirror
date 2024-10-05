2dce3e96ee8798ae191575533fe8c1a8d0261a5b
Lachlan Pease <830645+predakanga@users.noreply.github.com>
2024-10-05 22:34:20 +1000

    Added circuit breaker for errors during mirroring

--- worker.go
+++ worker.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"go.uber.org/zap"
 	"io"
+	"math"
+	"math/rand/v2"
 	"net/http"
 	"time"
 )
@@ -35,7 +37,26 @@ func sendRequest(req *http.Request, ctx context.Context, transport http.RoundTri
 	return nil
 }
 
+// Algorithm and defaults taken from
+// https://github.com/cenkalti/backoff/blob/v4/exponential.go
+const retryBaseInterval = float64(500 * time.Millisecond)
+const retryMultiplier = 1.5
+const retryRandomFactor = 0.5
+const retryMaximum = float64(60 * time.Second)
+
+func nextRetryIn(failures int) time.Duration {
+	multiplier := float64(max(failures-1, 0))
+	base := retryBaseInterval * math.Pow(retryMultiplier, multiplier)
+	randomDelta := base * retryRandomFactor
+	minInterval := base - randomDelta
+	maxInterval := base + randomDelta
+
+	return time.Duration(min(minInterval+(rand.Float64()*(maxInterval-minInterval+1)), retryMaximum))
+}
+
 func (m *Mirror) mirrorWorker() {
+	failures := 0
+	noRequestsUntil := time.Now()
 	// Create a RoundTripper to service all requests for this worker
 	transport := createRoundtripper()
 	baseCtx, cancel := context.WithCancel(context.Background())
@@ -46,9 +67,16 @@ func (m *Mirror) mirrorWorker() {
 		case _, _ = <-m.cancelChan:
 			return
 		case serReq := <-m.requestChan:
+			if failures > 0 && noRequestsUntil.After(time.Now()) {
+				continue
+			}
 			req := serReq.deserialize(m.parsedTarget)
 			if err := sendRequest(req, baseCtx, transport, m.RequestTimeout); err != nil {
-				m.logger.Error("Mirror request failed", zap.Error(err))
+				failures += 1
+				noRequestsUntil = time.Now().Add(nextRetryIn(failures))
+				m.logger.Error("Mirror request failed", zap.Error(err), zap.Time("backoff_until", noRequestsUntil))
+			} else {
+				failures = 0
 			}
 		}
 	}
